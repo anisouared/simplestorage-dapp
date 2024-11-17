21981b813be2c6e751a7fcc829cacb92158cb1a8
anisouared <ouared.anis@gmail.com>
2024-11-17 18:06:56 +0100

    First commit :)

---
+++ constants/index.js
@@ -0,0 +1,48 @@
+export const contractAddress = "0x28A9d726C4705140675b3dFFa0a68495B0A39730"
+export const contractAbi = [
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "oldValue",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "newValue",
+                "type": "uint256"
+            }
+        ],
+        "name": "NumberChanged",
+        "type": "event"
+    },
+    {
+        "inputs": [],
+        "name": "retrieve",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_number",
+                "type": "uint256"
+            }
+        ],
+        "name": "store",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
